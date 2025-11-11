a7a8bde5d89c8048e4a990cc9b2498d5b34ac590
Sourov-Ahmed26 <silenboyk7@gmail.com>
2025-11-11 14:54:35 +0600

    Vercel.json added

--- index.js
+++ index.js
@@ -63,9 +63,9 @@ async function run() {
             res.send(result);
         });
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
 
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // finish/error
         // await client.close();
